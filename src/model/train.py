bb02f2ff1fa041793e79844f99c2a5d01891cfed
Alex Beckwith <57322567+witzbeck@users.noreply.github.com>
2023-08-11 07:50:32 -0400

    lab1

--- src/model/train.py
+++ src/model/train.py
@@ -1,73 +1,131 @@
-# Import libraries
+from argparse import ArgumentParser
+from pathlib import Path
 
-import argparse
-import glob
-import os
 
-import pandas as pd
-
+from numpy import array
+from pandas import read_csv, concat, DataFrame
+from mlflow import autolog
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 
-# define functions
-def main(args):
-    # TO DO: enable autologging
+__args__ = {
+    "--training_data": {
+        "dest": "training_data",
+        "type": str,
+        "default": "production/data",
+    },
+    "--reg_rate": {
+        "dest": "reg_rate",
+        "type": float,
+        "default": 0.01,
+    },
+    "--random_state": {
+        "dest": "random_state",
+        "type": int,
+        "default": 0,
+    },
+    "--test_size": {
+        "dest": "test_size",
+        "type": float,
+        "default": 0.30,
+    },
+}
 
 
+def parse_args(
+        parser: ArgumentParser = ArgumentParser(),
+        args: dict = __args__,
+) -> ArgumentParser:
+    for key in args:
+        parser.add_argument(key, **args[key])
+    return parser.parse_args()
+
+
+def get_csv_df(path: Path) -> DataFrame:
+    print(path)
+    if not isinstance(path, Path):
+        path = Path(path)
+    if not path.exists():
+        raise FileExistsError(f"Cannot use non-existent path provided: {path}")
+    elif not (csv_files := list(path.rglob("*.csv"))):
+        raise RuntimeError(f"No CSV files found in provided data path: {path}")
+    elif (nfiles := len(csv_files)) == 0:
+        msg = f"Multiple CSV files found in provided data path: {path}"
+        raise RuntimeError(msg)
+    elif nfiles == 1:
+        return read_csv(csv_files[0])
+    return concat((read_csv(f) for f in csv_files), sort=False)
+
+
+def split_data(
+        df: DataFrame,
+        args: ArgumentParser,
+        xcols: list = [
+            "Pregnancies",
+            "PlasmaGlucose",
+            "DiastolicBloodPressure",
+            "TricepsThickness",
+            "SerumInsulin",
+            "BMI",
+            "DiabetesPedigree",
+            "Age"
+        ],
+        ycol: str = "Diabetic",
+) -> tuple:
+    X, y = df.loc[:, xcols].values, df.loc[:, ycol].values
+    return train_test_split(
+        X,
+        y,
+        random_state=args.random_state,
+        test_size=args.test_size,
+    )
+
+
+def train_model(
+        reg_rate: float,
+        X_train: array,
+        y_train: array,
+        solver: str = "liblinear",
+) -> LogisticRegression:
+    C = 1 / reg_rate
+    return LogisticRegression(
+        C=C,
+        solver=solver
+    ).fit(X_train, y_train)
+
+
+def main(
+        args: ArgumentParser = parse_args(),
+) -> LogisticRegression.decision_function:
+    print("starting log")
+    autolog()
     # read data
-    df = get_csvs_df(args.training_data)
+    print("reading csv")
+    df = get_csv_df(args.training_data)
 
     # split data
-    X_train, X_test, y_train, y_test = split_data(df)
-
-    # train model
-    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
-
-
-def get_csvs_df(path):
-    if not os.path.exists(path):
-        raise RuntimeError(f"Cannot use non-existent path provided: {path}")
-    csv_files = glob.glob(f"{path}/*.csv")
-    if not csv_files:
-        raise RuntimeError(f"No CSV files found in provided data path: {path}")
-    return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
-
-
-# TO DO: add function to split data
+    print("splitting data")
+    X_train, X_test, y_train, y_test = split_data(
+        df,
+        args,
+    )
+    print("training model")
+    res = train_model(
+        args.reg_rate,
+        X_train,
+        y_train
+    )
+    return res, X_test, y_test
 
 
-def train_model(reg_rate, X_train, X_test, y_train, y_test):
-    # train model
-    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
-
-
-def parse_args():
-    # setup arg parser
-    parser = argparse.ArgumentParser()
-
-    # add arguments
-    parser.add_argument("--training_data", dest='training_data',
-                        type=str)
-    parser.add_argument("--reg_rate", dest='reg_rate',
-                        type=float, default=0.01)
-
-    # parse args
-    args = parser.parse_args()
-
-    # return args
-    return args
-
-# run script
 if __name__ == "__main__":
     # add space in logs
     print("\n\n")
     print("*" * 60)
 
-    # parse args
-    args = parse_args()
-
     # run main function
-    main(args)
+    main()
 
     # add space in logs
     print("*" * 60)
